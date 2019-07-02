dc4d5af3381d9673c0d7b234f5958b61712c1b11
bambooka <sumkina.ol@yandex.ru>
2019-07-02 08:36:38 +0300

    feat: put data to component

---
+++ src/component/Tour/Tour.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './tour.scss'
+
+export default class Tour extends React.Component{
+    render(){
+        return (
+            <div>hello i am tour</div>
+        )
+    }
+}
---
+++ src/component/TourList/TourList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Tour from '../Tour/Tour'
+import './tourlist.scss'
+import {tourData} from '../../tourData'
+
+export default class TourList extends React.Component{
+    state = {
+        tours: tourData
+    };
+
+    removeTour(id){
+        console.log(id);
+    }
+
+
+    render(){
+        // console.log(this.state.tours)
+
+        const {tours} = this.state;
+        return (
+            <section className="tourlist">
+                {
+                    tours.map(tour => (
+                        <Tour key={tour.id} tour={tour} removeTour={this.removeTour}/>
+                    ))
+                }
+            </section>
+        )
+    }
+}
